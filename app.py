e9ea08d75be871dab8e756e935c995af85d4f056
Vitalii Vrublevskyi <vrublevskyi@fb.com>
2020-12-23 20:45:17 +0000

    Add simple routing and 2 pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 
 import json
 import logging
@@ -15,19 +15,19 @@ PORT = 5000
 app = Flask(__name__)
 logging.basicConfig(filename='movie_classifier.log', level=logging.DEBUG)
 model = joblib.load(MODEL)
-label = {0:'negative', 1:'positive'}
+label = {0: 'negative', 1: 'positive'}
 
 
-@app.route('/')
-def home():
-    return 'It works.'
+# @app.route('/')
+# def home():
+#     return 'It works.'
 
 
 def predict(model, text):
     return label[model.predict([text])[0]]
 
 
-@app.route('/review', methods=['GET'])
+@app.route('/review', methods=['POST'])
 def extract():
     """Return the movie review sentiment score.
     
@@ -36,16 +36,33 @@ def extract():
          "sentiment": "positive"
     }
     """
-    if request.method == 'GET':
-        description = request.args.get('text', '')
-        
+    if request.method == 'POST':
+        description = request.form.get('text', '')
+
         result = {
             'sentiment': predict(model, description)
         }
-        return json.dumps(result) 
+        # return json.dumps(result)
+        return render_template('result.html', prediction=result['sentiment'])
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         message = request.form['message']
+#         data = [message]
+#         vect = pd.DataFrame(cv.transform(data).toarray())
+#         body_len = pd.DataFrame([len(data) - data.count(" ")])
+#         punct = pd.DataFrame([count_punct(data)])
+#         total_data = pd.concat([body_len, punct, vect], axis=1)
+#         my_prediction = clf.predict(total_data)
+#     return render_template('result.html', prediction=my_prediction)
 
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=PORT)
-
